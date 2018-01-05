6a4ef1416635ce460366b7b0c3c5286efbc662fc
Aleksandr <aleksandr@Aleksandrs-MacBook-Pro.local>
2018-01-05 12:56:35 +0200

    initial

---
+++ cleaning.sql
@@ -0,0 +1,8 @@
+DELETE FROM movies_session WHERE show_id='301221'
+DELETE FROM movies_cinemamovie WHERE event_id='301221'
+DELETE FROM movies_movie WHERE event_id='301221'
+--DELETE FROM movies_movie WHERE event_id='301221'
+--DELETE FROM movies_cinemamovie WHERE event_id='301221'
+--DELETE FROM movies_cinemamovie WHERE event_id='301221'
+
+SELECT * FROM movies_movie
